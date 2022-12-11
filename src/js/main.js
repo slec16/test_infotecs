50b9bb88ddf73436d1b47b08a524b91c399f5b63
Alexandr Kopytov <sankopytov.76@mail.ru>
2022-12-12 02:31:06 +0700

    slide

--- src/js/main.js
+++ src/js/main.js
@@ -12,6 +12,7 @@ class ExpandableText {
     this.isToggle = false; 
      
     this.toggleBtn = document.createElement('button'); 
+    this.toggleBtn.className = "expandable-btn"
     this.toggleBtn.textContent = TogglePhrases.Expand; 
     this.toggleBtn.addEventListener('click', () => this.toggle()); 
      
@@ -38,10 +39,33 @@ class ExpandableText {
   } 
 } 
 
+
+
+
 window.onload = () => { 
+
   const textElems = document.querySelectorAll('.expandable-text'); 
    
   for (const el of textElems) { 
-    new ExpandableText(el, 100); 
+    new ExpandableText(el, 80); 
   } 
+
+
+  const smoothScrollLinks = document.querySelectorAll('.smooth-scroll'); 
+  for (let link of smoothScrollLinks) { 
+    link.addEventListener('click', event => { 
+      event.preventDefault(); 
+       
+      const target = event.target; 
+      const elementToScroll = document.querySelector(target.getAttribute('href')); 
+      elementToScroll.scrollIntoView({ behavior: 'smooth', block: 'end'}); 
+    }); 
+  } 
+
+ 
+
 }
+
+
+
+
